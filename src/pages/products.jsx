bf256bd826f2070eeb8b6713d7bf812431405316
root <root@Predator>
2024-08-29 10:41:38 +0530

    minior chage --added footer to products

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Navbar from "../components/Navbar";
 import Products from "../components/Products/Main";
+import Footer from "@/components/Footer";
 
 const products = () => {
   return (
@@ -8,6 +9,7 @@ const products = () => {
       <Navbar />
       <main>
         <Products />
+        <Footer />
       </main>
     </main>
   );
